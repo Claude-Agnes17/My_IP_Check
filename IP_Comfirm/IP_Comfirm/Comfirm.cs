65fbff6ed05a4322869a244c13b1ca93cc073623
gjjun <rlawnstjddl12@gmail.com>
2022-03-22 11:23:44 +0900

    Upload FIles
    Upload Files

---
+++ IP_Comfirm/IP_Comfirm/Comfirm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IP_Comfirm
+{
+    class Comfirm
+    {
+        static void Main(string[] args)
+        {
+            string localIP = string.Empty;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            Console.WriteLine("IP Address = " + localIP);
+            Console.ReadKey();
+        }
+    }
+}
